8a77e08c1393258e8bdfc9d41854373ce6f7396d
JayvHelderen <jayvanhelderen@hotmail.com>
2021-02-10 15:57:24 +0100

    Create CalculatorTest.java

--- src/test/java/com/pharmapartners/agenda/models/CalculatorTest.java
+++ src/test/java/com/pharmapartners/agenda/models/CalculatorTest.java
@@ -1,23 +1,20 @@
 package com.pharmapartners.agenda.models;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-@SpringBootTest
-public class CalculatorTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
 
     @Test
     void addTwoNumbers() {
         Calculator calculator = new Calculator();
-        double number1 = 15;
-        double number2 = 7;
-
+        double number1 = 10;
+        double number2 = 11;
+        double expectedAnswer = 21;
 
         double actualAnswer = calculator.addTwoNumbers(number1, number2);
-        double expectedAnswer = 22;
 
         assertEquals(expectedAnswer, actualAnswer);
     }
-
 }
