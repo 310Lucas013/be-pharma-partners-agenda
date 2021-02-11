bc40459fc6750ea17a5fb1c507122495d672e41f
JayvHelderen <jayvanhelderen@hotmail.com>
2021-02-11 12:24:43 +0100

    Update CalculatorTest.java

--- src/test/java/com/pharmapartners/agenda/models/CalculatorTest.java
+++ src/test/java/com/pharmapartners/agenda/models/CalculatorTest.java
@@ -22,7 +22,7 @@ class CalculatorTest {
     void addingOneNumberBelow_0_ShouldThrow_IllegalArgumentException() throws Exception{
         Calculator calculator = new Calculator();
         double number1 = 10;
-        double number2 = -3;
+        double number2 = 1;
 
         assertThrows(
                 IllegalArgumentException.class,
