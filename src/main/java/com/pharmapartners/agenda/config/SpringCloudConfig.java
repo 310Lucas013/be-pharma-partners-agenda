411c95e336b2c7b3b3b4fa1cf5f9a29ae47f3eab
i389746 <389746@stundent.fontys.nl>
2021-03-29 10:30:20 +0200

    gateway configuration

--- src/main/java/com/pharmapartners/agenda/config/SpringCloudConfig.java
+++ src/main/java/com/pharmapartners/agenda/config/SpringCloudConfig.java
@@ -1,5 +1,6 @@
 package com.pharmapartners.agenda.config;
 
+import reactor.core.publisher.Mono;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
